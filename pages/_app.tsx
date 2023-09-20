6a1d1e367afbada31aee5069ec657d268972c3d4
Carlos Zabala <czabala847@gmail.com>
2023-09-19 19:40:44 -0500

    add material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { lightTheme } from "../themes/light-theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
