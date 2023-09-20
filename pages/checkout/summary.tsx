cfbd82b53d13c8b9d2f17059daffa3ebc8d0ce1e
Carlos Zabala <czabala847@gmail.com>
2023-09-19 22:01:19 -0500

    checkout summary

---
+++ pages/checkout/summary.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Link from "next/link";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+import { ShopLayout } from "@/components/layouts";
+import { CartList, OrderSummary } from "@/components/cart";
+
+const SummaryPage = () => {
+  return (
+    <ShopLayout
+      title="Resumen de orden"
+      pageDescription={"Resumen de la orden"}
+    >
+      <Typography variant="h1" component="h1">
+        Resumen de la orden
+      </Typography>
+
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Resumen (3 productos)</Typography>
+              <Divider sx={{ my: 1 }} />
+
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="subtitle1">
+                  Dirección de entrega
+                </Typography>
+                <Link href="/checkout/address">Editar</Link>
+              </Box>
+
+              <Typography>Fernando Herrera</Typography>
+              <Typography>323 Algun lugar</Typography>
+              <Typography>Stittsville, HYA 23S</Typography>
+              <Typography>Canadá</Typography>
+              <Typography>+1 23123123</Typography>
+
+              <Divider sx={{ my: 1 }} />
+
+              <Box display="flex" justifyContent="end">
+                <Link href="/cart">Editar</Link>
+              </Box>
+
+              <OrderSummary />
+
+              <Box sx={{ mt: 3 }}>
+                <Button color="secondary" className="circular-btn" fullWidth>
+                  Confirmar Orden
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default SummaryPage;
