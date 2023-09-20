72750003c56dab8832a4f354e2f0f2fc6e7601c4
Carlos Zabala <czabala847@gmail.com>
2023-09-19 19:52:54 -0500

    add layout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Head from "next/head";
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+  imageFullUrl?: string;
+  pageDescription: string;
+  title: string;
+}
+
+export const ShopLayout: React.FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+
+      <nav></nav>
+
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0px 30px",
+        }}
+      >
+        {children}
+      </main>
+
+      <footer></footer>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import Typography from "@mui/material/Typography";
+
+import { ShopLayout } from "@/components/layouts";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+    <ShopLayout
+      title="Teslo-Shop - Home"
+      pageDescription="Encuentra los mejores productos de Teslo aqui"
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
   );
 }
