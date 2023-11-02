3d0198a947a012c627e397595a165b54427445ef
Carlos Zabala <czabala847@gmail.com>
2023-11-01 19:58:23 -0500

    middleware admin dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { IUser } from "./interfaces";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
+  const previousPage = req.nextUrl.pathname;
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   //   console.log(session);
@@ -18,10 +20,18 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if (
+    previousPage.startsWith("/admin") &&
+    (session.user as IUser).role !== "admin"
+  ) {
+    return NextResponse.redirect(new URL("/", req.url));
+    // return NextResponse.redirect("/");
+  }
+
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/checkout/address", "/checkout/summary"],
+  matcher: ["/checkout/address", "/checkout/summary", "/admin/:path*"],
 };
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Grid, Typography } from "@mui/material";
+import useSWR from "swr";
 import {
   AccessTimeOutlined,
   AttachMoneyOutlined,
@@ -14,8 +15,46 @@ import {
 
 import { AdminLayout } from "@/components/layouts";
 import { SummaryTile } from "@/components/admin";
+import { DashboardSummaryResponse } from "@/interfaces";
 
 const DashboardPage = () => {
+  const { data, error } = useSWR<DashboardSummaryResponse>(
+    "/api/admin/dashboard",
+    {
+      refreshInterval: 30 * 1000, // 30 segundos
+    }
+  );
+
+  const [refreshIn, setRefreshIn] = useState(30);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Tick");
+      setRefreshIn((refreshIn) => (refreshIn > 0 ? refreshIn - 1 : 30));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!error && !data) {
+    return <></>;
+  }
+
+  if (error) {
+    console.log(error);
+    return <Typography>Error al cargar la información</Typography>;
+  }
+
+  const {
+    numberOfOrders,
+    paidOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+    lowInventory,
+    notPaidOrders,
+  } = data!;
+
   return (
     <AdminLayout
       subTitle="Estadisticas generales"
@@ -24,43 +63,37 @@ const DashboardPage = () => {
     >
       <Grid container spacing={2}>
         <SummaryTile
-          // title={ numberOfOrders }
-          title={0}
+          title={numberOfOrders}
           subTitle="Ordenes totales"
           icon={<CreditCardOutlined color="secondary" sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          // title={ paidOrders }
-          title={0}
+          title={paidOrders}
           subTitle="Ordenes pagadas"
           icon={<AttachMoneyOutlined color="success" sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          // title={ notPaidOrders }
-          title={0}
+          title={notPaidOrders}
           subTitle="Ordenes pendientes"
           icon={<CreditCardOffOutlined color="error" sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          // title={ numberOfClients }
-          title={0}
+          title={numberOfClients}
           subTitle="Clientes"
           icon={<GroupOutlined color="primary" sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          // title={ numberOfProducts }
-          title={0}
+          title={numberOfProducts}
           subTitle="Productos"
           icon={<CategoryOutlined color="warning" sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          // title={ productsWithNoInventory }
-          title={0}
+          title={productsWithNoInventory}
           subTitle="Sin existencias"
           icon={
             <CancelPresentationOutlined color="error" sx={{ fontSize: 40 }} />
@@ -68,8 +101,7 @@ const DashboardPage = () => {
         />
 
         <SummaryTile
-          // title={ lowInventory }
-          title={0}
+          title={lowInventory}
           subTitle="Bajo inventario"
           icon={
             <ProductionQuantityLimitsOutlined
@@ -80,8 +112,7 @@ const DashboardPage = () => {
         />
 
         <SummaryTile
-          // title={ refreshIn }
-          title={0}
+          title={refreshIn}
           subTitle="Actualización en:"
           icon={<AccessTimeOutlined color="secondary" sx={{ fontSize: 40 }} />}
         />
